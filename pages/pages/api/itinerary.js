335ac41ae6138b22224b882a738315fe7c556458
prudhvikumar344-cmyk <prudhvikumar344@gmail.com>
2026-01-12 13:10:48 -0600

    Update itinerary.js
    Fix itinerary API

--- pages/pages/api/itinerary.js
+++ pages/pages/api/itinerary.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
+  // Only allow POST
   if (req.method !== "POST") {
-    res.setHeader("Allow", ["POST"]);
     return res.status(405).json({ error: "Method not allowed" });
   }
 
